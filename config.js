6314e5f406e5a85bd2c6725dc224e7acfc1f6a97
Icekingdom4 <ronniebigrigsale@gmail.com>
2025-01-02 11:13:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ require("./doc/module.js")
 */
 
 global.prefix = ['','!','.',',','馃悿','馃椏']
-global.owner = ['254742491666']
-global.ownMain = '254742491666'
+global.owner = ['2349095894070']
+global.ownMain = '2349095894070'
 global.NamaOwner = '饾暥饾枎饾枔饾枌 饾暰饾枂饾枓' //
 global.sessionName = 'session'
 global.connect = true // 
-global.namabot = 'CLASSIC-V4' //
+global.namabot = 'ICE-X' //
 global.author = '饾暥饾枎饾枔饾枌 饾暰饾枂饾枓' //
-global.packname = 'CLASSIC-V4' //
+global.packname = 'ICE-X' //
 global.url1 = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e' //
 global.url2 = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e' //
 global.linkgc = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
